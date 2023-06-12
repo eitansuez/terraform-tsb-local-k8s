2f29a77a5f192272bc5a6cc73d8b21329fc38888
Eitan Suez <eitan@tetrate.io>
2023-06-12 17:10:22 -0500

    for now test with a small vm

--- main.tf
+++ main.tf
@@ -17,7 +17,8 @@ provider "google" {
 
 resource "google_compute_instance" "tsb_vm" {
   name         = "tsb-vm"
-  machine_type = "e2-standard-8"
+  machine_type = "e2-medium" # for testing..
+  #machine_type = "e2-standard-8" # ultimately
 
   tags         = ["http-server", "https-server"]
 
