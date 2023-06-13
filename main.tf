318a69fa095d9d717efbb0bf2ce9fee2f93bdad8
Eitan Suez <eitan@tetrate.io>
2023-06-13 12:16:33 -0500

    adds back ssh key pair for access

--- main.tf
+++ main.tf
@@ -15,6 +15,11 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "google_compute_instance" "tsb_vm" {
   name         = "tsb-vm"
   machine_type = "e2-standard-8"
@@ -39,6 +44,21 @@ resource "google_compute_instance" "tsb_vm" {
     user-data = templatefile("${path.module}/vm-userdata.tftpl", {
       tsb_image_sync_username = var.tsb_image_sync_username
       tsb_image_sync_apikey   = var.tsb_image_sync_apikey
+      pubkey                  = tls_private_key.generated.public_key_openssh
     })
   }
+}
+
+resource "local_file" "tsbadmin_pem" {
+  filename        = "gcp-ubuntu.pem"
+  file_permission = "0600"
+  content         = tls_private_key.generated.private_key_pem
+  depends_on      = [tls_private_key.generated]
+}
+
+resource "local_file" "ssh_jumpbox" {
+  filename        = "ssh-to-gcp-vm.sh"
+  file_permission = "0755"
+  content         = "ssh -i gcp-ubuntu.pem ubuntu@${google_compute_instance.tsb_vm.network_interface[0].access_config[0].nat_ip}"
+  depends_on      = [local_file.tsbadmin_pem]
 }
